f1d4a1a70fffec1e3c8d8507377fa0139c637d7d
Barney <brguo55@gmail.com>
2024-04-10 19:27:38 -0400

    screen 2 and screen 3(part)

--- main.py
+++ main.py
@@ -11,4 +11,24 @@ while True:
     x) to exit
     ''')
 
+# process user input
+    if file_variable == 'x':
+        sys.exit() # this requires
+    elif file_variable == 'a':
+        file_variable = '60s-music.csv'
+    elif file_variable == 'b':
+        file_variable= 'athletes.csv'
+    else:
+        print("Invalid option. Please select a, b, or x")
+        continue
     
+    # enter a search term this // is a global variable
+    search_variable = input(f"Enter the serach term for {file_variable} file: ")
+    search_variable = search_variable.title() # Make it so that the user can enter lower-case term.
+    
+
+# go to 02_search_for_term.py to continue .....
+def find(file_variable, search_variable):
+    with open(file_variable, "r") as file:
+        content = file.read()
+# now the file is in the memory buffer as content
